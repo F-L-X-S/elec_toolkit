5958c2aa6147e19143b4fb297058e42321210ec0
F-L-X-S <felixschuelke@icloud.com>
2023-06-17 22:48:12 +0200

    added plot-function to 'Complex_phys_unit'-class, added 'exmpl_plot_vector'

--- Complex_phys_unit.py
+++ Complex_phys_unit.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Complex_phys_unit(Phys_unit):
-    def __init__(self, value, unit, discription=""):
+    def __init__(self, value: complex, unit, discription=""):
         self.value = value
         self.unit = unit
         self.discr = discription
         self.magn = round(self.complex_to_polar(value)['magn'], 3)
         self.phase = round(self.complex_to_polar(value)["phase"], 3)
+        self.origin = 0+0j
         super().__init__(value, unit, discription)
 
     @classmethod 
@@ -36,3 +37,29 @@ class Complex_phys_unit(Phys_unit):
         magn = round(magn, 3)
         return {"magn":magn, "phase":phase}
     
+    #Function to plot vector with annotation 
+    @staticmethod
+    def plot_vector(vector, origin=[0, 0],name = "", unit = "", **options):
+        #calculate absolute vector-end
+        vector_end=[vector[0]+origin[0], vector[1]+origin[1]]
+        #calculate magnitude
+        magn = np.sqrt(((vector_end[0]-origin[0])**2)+((vector_end[1]-origin[1])**2))
+        #calculate rel. position of annotations
+        annotation_pos = [origin[0]+0.7*(vector_end[0]-origin[0]), origin[1]+0.7*(vector_end[1]-origin[1])]
+        offset = ( -0.2*magn,0.1*magn )
+        #annotate vector
+        plt.annotate(name+ " ["+unit+ "]", (annotation_pos[0],annotation_pos[1]),xycoords='data',
+                 xytext=offset, textcoords='offset points')
+        #plot vector 
+        return plt.arrow(origin[0], origin[1], vector[0], vector[1],
+            head_width=0.05*magn, head_length=0.1*magn, length_includes_head=True,
+            width=0.002*magn, 
+            **options)
+    
+    
+    def plot(self, conc_vector=None, **options):
+        if conc_vector == None:
+            self.origin = 0+0j
+        else:
+            self.origin = conc_vector.origin + conc_vector.value
+        return self.plot_vector([self.value.real, self.value.imag], [self.origin.real, self.origin.imag], self.discr, self.unit, **options)
---
+++ exmpl_plot_vector.py
@@ -0,0 +1,28 @@
+from Complex_phys_unit import *
+
+#This example shows, how to plot complex values as a vector diagram with the Complex_phys_unit class
+
+#create voltage-objects from magnitude and phase 
+V1 = Complex_phys_unit.from_polar(60, 0, "V", "V1")
+V2 = Complex_phys_unit.from_polar(60, -120, "V", "V2")
+V3 = Complex_phys_unit.from_polar(60, +120, "V", "V3")
+
+#create current-object from real- and imaginary part
+I1 = Complex_phys_unit(10+20j, "A", "I1")
+
+#plot the first vector with origin [0, 0]
+V1.plot() 
+#concatenate the second vector with the previous one 
+V2.plot(V1) 
+#concatenate the second vector with the previous one 
+V3.plot(V2)
+
+#plot the current without concatenation
+print(I1)
+#configure plot (check matplotlib-docs for details)
+plt.title("three-phase system")
+plt.ylabel('Im')
+plt.xlabel('Re')
+plt.grid()
+plt.gca().set_aspect("equal")
+plt.show()
